155d78648cf6ee07c8af4e1d78a609e0cf1e6dce
Brian Qiu <brian.qiu9522@hotmail.com>
2021-09-20 13:09:08 -0400

    fixed port for deployment

--- app/blog/blog.py
+++ app/blog/blog.py
@@ -1,7 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session, Blueprint , flash, make_response
 
-
-
 blog_page_bp = Blueprint('blog_page_bp', __name__, template_folder = 'templates')
 
 
